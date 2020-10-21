6b1fc38fdf1a69dfff9c6bbeb4d6cc39c5594da8
Farhan Kassam <farhankassam2@gmail.com>
2020-10-21 01:13:35 -0700

    committing all code files, including the added package react-native-branch.

---
+++ android/app/src/main/java/com/pnm_179_test_project/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pnm_179_test_project;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "PNM_179_Test_Project";
+  }
+}
